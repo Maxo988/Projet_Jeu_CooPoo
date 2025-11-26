a42ba78eda37c9af6cb3a0b3f6a65e7740b2c3b0
Marie <mterrasaquantin@gmail.com>
2025-11-26 15:59:51 +0100

    début code objets

--- src/Objets/UsageUnique.java
+++ src/Objets/UsageUnique.java
@@ -1,8 +1,9 @@
 package Objets;
 
 public class UsageUnique extends Item {
+    protected boolean utilise = false;
+    public UsageUnique(String Nom, String Description) {
+    super(Nom, Description);
 
-    public UsageUnique() {
-        super(1, "oui", "non", 5);
     }
 }
