c160d635de5a3b52e19197bbe2392835353e45c2
Guillaume Campistron <guillaume.campistron@hotmail.fr>
2025-11-26 15:22:34 +0100

    main test

--- src/Generators/GeneratorTresor.java
+++ src/Generators/GeneratorTresor.java
@@ -1,7 +1,34 @@
 package Generators;
 
-import Java.util;
+import java.util.Random;
+import java.util.ArrayList;
 
 public class GeneratorTresor {
+
+    private ArrayList<Integer> Num;
+    private int etage;
+    public GeneratorTresor()
+    {
+        Num = new ArrayList<Integer>();
+        for(int i=0; i<4; i++)
+        {
+            Num.add(i);
+        }
+    }
+
+    public void printList()
+    {
+        for(int i : Num)
+        {
+            System.out.println(i);
+        }
+    }
+
+    public int tirage(int etage)
+    {
+        Random randomGenerator = new Random();
+        int choixTresor = randomGenerator.nextInt(4);
+        return(choixTresor);
+    }
 
 }
