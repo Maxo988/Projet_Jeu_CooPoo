80ecdf7e9807c18a5879f0e82690df22e0b5863f
Guillaume Campistron <guillaume.campistron@hotmail.fr>
2025-11-26 15:09:02 +0100

    main test

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import Generators.GeneratorTresor;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args){
+        GeneratorTresor gen;
+        gen = new GeneratorTresor();
+        gen.printList();
+    }
+}
