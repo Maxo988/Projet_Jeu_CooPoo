084aaebc47305d9e64f6a30431c44a84be93fcfd
Maxo988 <maelzamitb@gmail.com>
2025-11-26 11:07:22 +0100

    Premier Push

--- src/Joueur/Joueur.java
+++ src/Joueur/Joueur.java
@@ -1,10 +1,12 @@
 package Joueur;
-import java.util.random;
+import Objets.Arme;
+
+import java.util.Random;
 import java.util.ArrayList;
 
 public class Joueur {
     private int currentHealth;
     private int maxHealth;
-    private Weapon weapon = Fist;
+    private Arme arme = Fist;
 
 }
