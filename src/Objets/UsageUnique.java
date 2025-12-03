f624fb51880ff1ff88d487139689f252ed3e9bb2
Marie <mterrasaquantin@gmail.com>
2025-12-03 16:57:44 +0100

    modif package objets

--- src/Objets/UsageUnique.java
+++
@@ -1,9 +0,0 @@
-package Objets;
-
-public class UsageUnique extends Item {
-    protected boolean utilise = false;
-    public UsageUnique(String Nom, String Description) {
-    super(Nom, Description);
-
-    }
-}
