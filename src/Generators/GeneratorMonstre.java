31dffef898d54572f48e415bad56d62bedba468d
Guillaume Campistron <guillaume.campistron@hotmail.fr>
2025-12-03 16:07:10 +0100

    modif generateur monstre

--- src/Generators/GeneratorMonstre.java
+++ src/Generators/GeneratorMonstre.java
@@ -1,55 +1,81 @@
 package Generators;
+
 import java.util.ArrayList;
 import java.util.Random;
-
 import Monstres.Comportement;
 import Monstres.Monstre;
 import Monstres.Boss;
 import Objets.Arme;
 import Objets.Item;
+import Environement.Etage;
 
 public class GeneratorMonstre {
     private ArrayList<Monstre> listMonstre = new ArrayList<>();
-
+    Random randomGenerator = new Random();
 
     public GeneratorMonstre()
     {
-
     }
 
-    public void initMonstre() {
-        Monstre Ambystoma = new Monstre(
-                "Ambystoma",
-                1,
-                0,
-                "Un petit animal a l’air inoffensif et tout mignon … Vous n’allez quand même pas faire ce à quoi je pense ?",
-                "taser",
-                Comportement.INOFFENSIF,
-                "Espèce rare d’axolotl, animaux de compagnie de l’IA. Il ne vous attaquera pas, ne se défendra pas.",
-                100);
-        Monstre Cotylorhiza = new Monstre(
-                "Cotylorhiza",
-                100,
-                10,
-                "Sorte de méduse ressemblant à un oeuf au plat volant",
-                "Lance harpon",
-                Comportement.DEFENSIF,
-                "Avec son air gluant et ses tentacules menaçantes, il ne faut probablement pas s’en approcher. Une arme à distance comme un lance-harpon serait plus efficace.",
-                100);
-        Monstre Cornuta = new Monstre(
-                "Cornuta",
-                150,
-                "Description à définir.",
-                "Torche",
-                Comportement.AGRESSIF,
-                "Baudroie géante mutée, bien qu’elle émette de la lumière elle même, elle la craint comme la peste."
-                100
-        );
-    }
-
-    public Monstre genereMonstre()
+    public Monstre genereMonstre(int difficulte)
     {
+        int somme = 0;
+        int poidsTotal = 0;
+        int poidsActuel = 0;
 
+        switch(difficulte)
+        {
+            case 1:
+                Monstre Ambystoma = new Monstre(
+                        "Ambystoma",
+                        1,
+                        0,
+                        "Un petit animal a l’air inoffensif et tout mignon … Vous n’allez quand même pas faire ce à quoi je pense ?",
+                        "taser",
+                        Comportement.INOFFENSIF,
+                        "Espèce rare d’axolotl, animaux de compagnie de l’IA. Il ne vous attaquera pas, ne se défendra pas.",
+                        100);
+                Monstre Cotylorhiza = new Monstre(
+                        "Cotylorhiza",
+                        100,
+                        10,
+                        "Sorte de méduse ressemblant à un oeuf au plat volant",
+                        "Lance harpon",
+                        Comportement.DEFENSIF,
+                        "Avec son air gluant et ses tentacules menaçantes, il ne faut probablement pas s’en approcher. Une arme à distance comme un lance-harpon serait plus efficace.",
+                        2);
+                Monstre Cornuta = new Monstre(
+                        "Cornuta",
+                        150,
+                        "Description à définir.",
+                        "Torche",
+                        Comportement.AGRESSIF,
+                        "Baudroie géante mutée, bien qu’elle émette de la lumière elle même, elle la craint comme la peste."
+                        3);
+                listMonstre.add(Ambystoma);
+                listMonstre.add(Cotylorhiza);
+                listMonstre.add(Cornuta);
+
+                for(Monstre monstre :  listMonstre)
+                {
+                    poidsTotal += monstre.getPoids();
+                }
+                int tirage = randomGenerator.nextInt(3);
+                tirage +=1;
+                for(Monstre monstre : listMonstre)
+                {
+                    poidsActuel += monstre.getPoids();
+                    if(poidsActuel >= tirage)
+                    {
+                        return(monstre);
+                    }
+                }
+                ;break;
+            case 2:
+                ;break;
+            case 3:
+                ;break;
+        }
     }
 
 }
