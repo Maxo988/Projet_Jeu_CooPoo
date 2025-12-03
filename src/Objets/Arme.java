993c461510196df9cf0aec423603ea296a035ea8
Marie <mterrasaquantin@gmail.com>
2025-12-03 16:56:29 +0100

    modif package objets

--- src/Objets/Arme.java
+++ src/Objets/Arme.java
@@ -10,15 +10,15 @@ public class Arme extends Equipement {
     }
 
     public void utiliser(Monstre monstre){
-        if (this.nom = monstre.getPointFaible()) {
-            monstre.perdrePv(degats*1.5);
+        if (this.nom.equalsIgnoreCase(monstre.getPointFaible())) {
+            monstre.perdrePv((int) (degats*1.5));
             System.out.println("Bien joué, vous avez trouvé son point faible, " +
                     " le monstre semble vraiment sonné par votre attaque.");
         }
         else {
             monstre.perdrePv(degats);
             System.out.println("Votre attaque a fonctionné mais aurait été plus efficace avec " +
-                    " une autre arme".);
+                    " une autre arme.");
         }
     }
 
